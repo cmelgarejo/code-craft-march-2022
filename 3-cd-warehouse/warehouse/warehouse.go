fc095ed718f06e3cc3c4f62bd43850e1983c84b1
cmelgarejo <cmelgarejo.dev@gmail.com>
2022-03-15 08:38:58 -0300

    Encapsulate the stock in warehouse
    Signed-off-by: cmelgarejo <cmelgarejo.dev@gmail.com>

--- 3-cd-warehouse/warehouse/warehouse.go
+++ 3-cd-warehouse/warehouse/warehouse.go
@@ -9,28 +9,28 @@ const (
 )
 
 type Warehouse struct {
-	Stock []cd.CD
+	stock []cd.CD
 }
 
 func NewWarehouse(cds []cd.CD) *Warehouse {
 	return &Warehouse{
-		Stock: cds,
+		stock: cds,
 	}
 }
 
 func (w *Warehouse) AddCDs(cds []cd.CD) string {
 	for i, cd := range cds {
 		if result := w.SearchByTitle(cd.Title); result == NOT_FOUND {
-			w.Stock = append(w.Stock, cd)
+			w.stock = append(w.stock, cd)
 		} else if result == SUCCESS {
-			w.Stock[i].Quantity += cd.Quantity
+			w.stock[i].Quantity += cd.Quantity
 		}
 	}
 	return SUCCESS
 }
 
 func (w *Warehouse) SearchByTitle(title string) string {
-	for _, cd := range w.Stock {
+	for _, cd := range w.stock {
 		if cd.Title == title {
 			return SUCCESS
 		}
@@ -39,7 +39,7 @@ func (w *Warehouse) SearchByTitle(title string) string {
 }
 
 func (w *Warehouse) SearchByArtist(artist string) string {
-	for _, cd := range w.Stock {
+	for _, cd := range w.stock {
 		if cd.Artist == artist {
 			return SUCCESS
 		}
@@ -51,7 +51,7 @@ func (w *Warehouse) Rate(title string, rating int, comment string) string {
 	if result := w.SearchByTitle(title); result == NOT_FOUND {
 		return result
 	}
-	for _, cd := range w.Stock {
+	for _, cd := range w.stock {
 		if cd.Title == title {
 			cd.Rating += rating
 			cd.Comments = append(cd.Comments, comment)
@@ -65,7 +65,7 @@ func (w *Warehouse) Buy(title string, ccNo string, ccExpDate string) string {
 	if result := w.SearchByTitle(title); result == NOT_FOUND {
 		return result
 	}
-	for _, cd := range w.Stock {
+	for _, cd := range w.stock {
 		if cd.Title == title {
 			if cd.Quantity > 0 {
 				cd.Quantity--
